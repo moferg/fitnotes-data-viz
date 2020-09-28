bcb472579fbf3e375d48ebeb18926e155a100778
Marshall Ferguson <moferg01@louisville.edu>
2020-09-28 17:36:46 -0400

    added list of features to implement

--- fitnotes_data_viz.py
+++ fitnotes_data_viz.py
@@ -1,1 +1,14 @@
 # Marshall Ferguson - 9/2020
+
+# Features to implement:
+# Infinite main loop for user to choose options from
+# Implement a log to store errors in another file
+# Buid a conversion tool (lb to kg and vice-versa)
+# Read data from an external CSV file
+# Visualize data in a graph
+# Use matplotlib 
+
+# Potential features to include:
+# Create a dict or list and use it in program
+# Create and call at least 3 functions
+# Create 3 or more unit tests
