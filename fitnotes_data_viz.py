22c14acc8d6c53176ddbfdd68bf7377c9d13a85a
Marshall Ferguson <moferg01@louisville.edu>
2020-09-30 10:51:27 -0400

    added loop to go through csv file and output data

--- fitnotes_data_viz.py
+++ fitnotes_data_viz.py
@@ -12,3 +12,29 @@
 # Create a dict or list and use it in program
 # Create and call at least 3 functions
 # Create 3 or more unit tests
+
+# Imports
+
+import csv
+import matplotlib as plt
+import time
+
+print('Welcome to the FitNotes Data Visualizer!')
+time.sleep(1)
+print('This application will take in a CSV file from the FitNotes workout app,')
+time.sleep(2)
+print('and output graphs and visualizations, guiding you to a more data driven approach to working out.')
+time.sleep(2)
+
+with open('FitNotes_Export_2020_09_29.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names: {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\tOn {row[0]}, you worked out your {row[2]}.')
+            print(f'\tYou performed {row[1]} for {row[4]} reps at {row[3]} lbs.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
