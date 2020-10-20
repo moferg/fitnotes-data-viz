bee1f1fffff66aa73ba571bb00f367f23133506b
Marshall Ferguson <moferg01@louisville.edu>
2020-10-20 10:25:11 -0400

    added descriptive comments to code blocks and docstrings to functions

--- fitnotes_data_viz.py
+++ fitnotes_data_viz.py
@@ -22,10 +22,12 @@ import time
 
 # Functions
 
-def unique_values(list1): 
+def unique_values(list1):
+    '''Takes a list as input and outputs a list with all duplicates removes''' 
     x = np.array(list1) 
     return list(np.unique(x))
 
+# Create an welcoming menu for the user
 print('Welcome to the FitNotes Data Visualizer!')
 time.sleep(1)
 print('This application will take in a CSV file from the FitNotes workout app,')
@@ -33,9 +35,11 @@ time.sleep(2)
 print('and output graphs and visualizations, guiding you to a more data driven approach to working out.')
 time.sleep(2)
 
+# Variables created before opening CSV file
 dates = []
 volumes = []
 
+# Open CSV file and extract needed data
 with open('FitNotes_Export_2020_10_04.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -54,35 +58,42 @@ with open('FitNotes_Export_2020_10_04.csv') as csv_file:
     # print(volumes)
     # print(len(volumes))
 
+    # Remove duplicates from dates list
     unique_dates = unique_values(dates)
     # print(unique_dates)
     # print(len(unique_dates))
 
+    # Create a list of the number of sets performed on a certain date
     sets_on_date = []
     for date in unique_dates:
         sets_on_date.append(dates.count(date))
     # print(sets_on_date)
     # print(len(sets_on_date))
 
+    # Create a list of lists containing the volumes lifted on dates
     volumes_for_workout = []
     for sets in sets_on_date:
         volumes_for_workout.append(volumes[0:sets])
     # print(volumes_for_workout)
     # print(len(volumes_for_workout))
 
+    # Create a list containing the total volume of all exercises on certain dates
     volumes_on_unique_dates = []
     for volume in volumes_for_workout:
         volumes_on_unique_dates.append(sum(volume))
     # print(volumes_on_unique_dates)
     # print(len(volumes_on_unique_dates))
 
-    fig, ax = plt.subplots()
+    #TODO: change graph options so that it shows up larger by default
+    
+    # Create and display a chart with matplotlib graphing volume listed over time
+    ax = plt.axes()
     ax.bar(unique_dates, volumes_on_unique_dates)
     ax.scatter(unique_dates, volumes_on_unique_dates, color='r', linewidth=4)
     ax.plot(unique_dates, volumes_on_unique_dates, color='r', linewidth=2)
     for i in range(0, len(unique_dates)):
-        ax.annotate(str(volumes_on_unique_dates[i]), xy = (i - .25, volumes_on_unique_dates[i]))
+        ax.annotate(str(volumes_on_unique_dates[i]) + 'lbs', xy = (i - .35, volumes_on_unique_dates[i]))
     ax.set_xlabel('Workout Dates')
-    ax.set_ylabel('Workout Volumes')
+    ax.set_ylabel('Workout Volumes (lbs)')
     ax.set_title('Workout Volumes Over Time')
     plt.show()
