72b92c96793335f4cbe5b2a8e13487a6fff94a86
Marshall Ferguson <moferg01@louisville.edu>
2020-10-21 10:12:00 -0400

    added test_sets_on_date() and test_volumes_on_unique_dates()

--- test_fitnotes_data_viz.py
+++ test_fitnotes_data_viz.py
@@ -10,6 +10,15 @@ class TestFitnotesDataViz(unittest.TestCase):
     def test_unique_values(self):
         self.assertEqual(fitnotes_data_viz.unique_values([1, 1, 2, 2, 3, 3]), [1, 2, 3])
 
+    def test_sets_on_date(self):
+        self.assertEqual(fitnotes_data_viz.sets_on_date, [18, 27, 20, 27, 28, 20, 26, 27, 19, 27, 28, 19, 25, 28, 19, 28, 30, 20])
+
+    # def test_volumes_for_workout(self):
+    #     self.assertEqual(fitnotes_data_viz.volumes_for_workout, [])
+
+    def test_volumes_on_unique_dates(self):
+        self.assertEqual(fitnotes_data_viz.volumes_on_unique_dates, 
+        [10470, 11905, 13925, 17905, 14780, 15470, 20020, 14280, 16380, 21010, 15280, 17230, 22395, 16265, 17715, 22840, 16565, 17740])
 
 if __name__ == '__main__':
     unittest.main()
