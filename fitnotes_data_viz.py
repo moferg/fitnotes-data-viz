4a51eaa5d24ccb1f7c06d8f44dc0e77e04001464
Marshall Ferguson <moferg01@louisville.edu>
2020-10-23 16:13:25 -0400

    added variables for reps, muscle group, and exercise name

--- fitnotes_data_viz.py
+++ fitnotes_data_viz.py
@@ -37,6 +37,9 @@ time.sleep(2)
 
 # Variables created before opening CSV file
 dates = []
+reps = []
+muscle_group = []
+exercise_name = []
 volumes = []
 
 # Open CSV file and extract needed data
@@ -50,13 +53,16 @@ with open('FitNotes_Export_2020_10_04.csv') as csv_file:
         else:
             # print(f'\tOn {row[0]}, you performed {row[1]} for {row[4]} reps at {row[3]} lbs.')
             dates.append(row[0])
-            volumes.append(float(row[4]) * float(row[3]))
+            reps.append(int(row[4]))
+            muscle_group.append(row[2])
+            exercise_name.append(row[1])
+            volumes.append(float(row[4]) * float(row[3]))    # number of reps per set * number of lbs per rep
             line_count += 1
     # print(f'Processed {line_count} lines.')
-    # print(dates)
-    # print(len(dates))
-    # print(volumes)
-    # print(len(volumes))
+    print(f'Contents of dates variable: {dates}')
+    print(f'Length of dates variable: {len(dates)}')
+    print(f'Contents of volumes variable: {volumes}')
+    print(f'Length of volumes variable: {len(volumes)}')
 
     # Remove duplicates from dates list
     unique_dates = unique_values(dates)
@@ -65,13 +71,12 @@ with open('FitNotes_Export_2020_10_04.csv') as csv_file:
 
     # Create a list of the number of sets performed on a certain date
     sets_on_date = [dates.count(date) for date in unique_dates]
-    print(f'Contents of sets_on_dates variable: {sets_on_date}')
-    print(f'Length of sets_on_dates variable: {len(sets_on_date)}')
+    print(f'Contents of sets_on_date variable: {sets_on_date}')
+    print(f'Length of sets_on_date variable: {len(sets_on_date)}')
 
-    # TODO: double check and make sure this is assigning correcting volumes to list
-
+    # TODO: double check and make sure this is assigning correct volumes to list (IT IS NOT)
     # Create a list of lists containing the volumes lifted on dates
-    volumes_for_workout = [volumes[0:sets] for sets in sets_on_date]
+    volumes_for_workout = [volumes[:sets] for sets in sets_on_date]
     print(f'Contents of volumes_for_workout variable: {volumes_for_workout}')
     print(f'Length of volumes_for_workout variable: {len(volumes_for_workout)}')
 
@@ -84,13 +89,13 @@ with open('FitNotes_Export_2020_10_04.csv') as csv_file:
     # TODO: add body part targeted of each date to graph
 
     # Create and display a chart with matplotlib graphing volume listed over time
-    ax = plt.axes()
-    ax.bar(unique_dates, volumes_on_unique_dates)
-    ax.scatter(unique_dates, volumes_on_unique_dates, color='r', linewidth=4)
-    ax.plot(unique_dates, volumes_on_unique_dates, color='r', linewidth=2)
-    for i in range(0, len(unique_dates)):
-        ax.annotate(str(volumes_on_unique_dates[i]) + 'lbs', xy = (i - .35, volumes_on_unique_dates[i]))
-    ax.set_xlabel('Workout Dates')
-    ax.set_ylabel('Workout Volumes (lbs)')
-    ax.set_title('Workout Volumes Over Time')
-    plt.show()
+    # ax = plt.axes()
+    # ax.bar(unique_dates, volumes_on_unique_dates)
+    # ax.scatter(unique_dates, volumes_on_unique_dates, color='r', linewidth=4)
+    # ax.plot(unique_dates, volumes_on_unique_dates, color='r', linewidth=2)
+    # for i in range(0, len(unique_dates)):
+    #     ax.annotate(str(volumes_on_unique_dates[i]) + 'lbs', xy = (i - .35, volumes_on_unique_dates[i]))
+    # ax.set_xlabel('Workout Dates')
+    # ax.set_ylabel('Workout Volumes (lbs)')
+    # ax.set_title('Workout Volumes Over Time')
+    # plt.show()
